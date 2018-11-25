6430e420aa12f938942211a51acc95d3c28b8721
prost <prosto.steve123@gmail.com>
2018-11-25 21:16:58 +0500

    Создан файл, который заносит задаваемые вопросы в БД

---
+++ question.php
@@ -0,0 +1,9 @@
+<?php
+$question=$_GET["question"];
+
+$mysqli = new mysqli("localhost", "redalert1917", "1215100902000401", "themartianist") or die('Не могу выбрать базу данных');
+$sql = "INSERT INTO questions (question) VALUES ('$question')";
+if ($mysqli->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $mysqli->error;
