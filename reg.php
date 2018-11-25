a1219fea8cdb892bc1eab64fd729a745408f508d
prost <prosto.steve123@gmail.com>
2018-11-25 21:24:37 +0500

    Отредактирован файл регистрации Теперь добавляется в БД добавляются значения "email"

--- reg.php
+++ reg.php
@@ -1,10 +1,11 @@
 <?php
 $f_name=$_GET["f_name"];
 $l_name=$_GET["l_name"];
+$email=$_GET["email"];
 $role=$_GET["role"];
 
 $mysqli = new mysqli("localhost", "redalert1917", "1215100902000401", "themartianist") or die('Не могу выбрать базу данных');
-$sql = "INSERT INTO users (f_name, l_name, role) VALUES ('$f_name', '$l_name', '$role')";
+$sql = "INSERT INTO users (f_name, l_name, email,  role) VALUES ('$f_name', '$l_name', '$email', '$role')";
 if ($mysqli->query($sql) === TRUE) {
     echo "New record created successfully";
 } else {
