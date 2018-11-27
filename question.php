4f59a8be44f90532509d4f53354c5aba3a3d03fa
prost <prosto.steve123@gmail.com>
2018-11-27 22:06:15 +0500

    Отредактирован файл добавления вопросов Теперь добавляется в БД добавляются значения "name" и "category"

--- question.php
+++ question.php
@@ -1,8 +1,10 @@
 <?php
+$name=$_GET["name"];
+$category=$_GET["category"];
 $question=$_GET["question"];
 
 $mysqli = new mysqli("localhost", "redalert1917", "1215100902000401", "themartianist") or die('Не могу выбрать базу данных');
-$sql = "INSERT INTO questions (question) VALUES ('$question')";
+$sql = "INSERT INTO questions (name, category, question) VALUES ('$name', '$category', '$question')";
 if ($mysqli->query($sql) === TRUE) {
     echo "New record created successfully";
 } else {
